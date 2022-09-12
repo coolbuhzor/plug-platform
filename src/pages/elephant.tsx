3d49d6fb2643251494bc4a3cca7c35e934b4c06a
coolbuhzor <coolbuhzor@gmail.com>
2022-09-12 12:22:05 +0100

    mobile optiisation

--- src/pages/elephant.tsx
+++ src/pages/elephant.tsx
@@ -41,37 +41,19 @@ const Elephant = () => {
               className="w-full h-full object-fill"
             />
           </div>
-          <div className="flex w-full items-center pb-[16px] border-b border-[rgba(196,205,213,0.62)]">
-            <p className="font-overpass font-bold text-[48px] leading-[61px] text-[#30425A]">
+          <div className="flex flex-col w-full items-left pb-[16px] border-b border-[rgba(196,205,213,0.62)]">
+            <p className="font-overpass font-bold text-[3rem] leading-[61px] text-[#30425A]">
               {data?.name}
             </p>
-            <span className="font-overpass font-normal text-[14px] leading-[23px] ml-[23px] text-[#30425A]">
+            <span className="font-overpass font-normal text-[0.875rem] leading-[23px] lg:ml-[23px] text-[#30425A]">
               {data?.sex}
             </span>
           </div>
-          <p className="w-full pt-[20px] font-normal text-[24px] leading-[30px] text-[#30425A] font-overpass mb-[100px]">
+          <p className="w-full pt-[1.25rem] font-normal text-[1.4rem] leading-[1.875rem] text-[#30425A] font-overpass mb-[100px]">
             {data?.note}
           </p>
         </>
       )}
-      {/* <div className="w-full h-[431px] mb-[43px] border">
-        <img
-          src={data?.image}
-          alt={data?.name}
-          className="w-full h-full object-fill"
-        />
-      </div>
-      <div className="flex w-full items-center pb-[16px] border-b border-[rgba(196,205,213,0.62)]">
-        <p className="font-overpass font-bold text-[48px] leading-[61px] text-[#30425A]">
-          {data?.name}
-        </p>
-        <span className="font-overpass font-normal text-[14px] leading-[23px] ml-[23px] text-[#30425A]">
-          {data?.sex}
-        </span>
-      </div>
-      <p className="w-full pt-[20px] font-normal text-[24px] leading-[30px] text-[#30425A] font-overpass mb-[100px]">
-        {data?.note}
-      </p> */}
     </section>
   );
 };
